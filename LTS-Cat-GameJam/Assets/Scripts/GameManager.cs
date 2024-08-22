1c65ca16800fb09ac443d2f262d122adcf17efa3
Hman26 <harrisonwladis@gmail.com>
2024-08-22 12:08:16 -0400

    Added Star Assets to game

--- LTS-Cat-GameJam/Assets/Scripts/GameManager.cs
+++ LTS-Cat-GameJam/Assets/Scripts/GameManager.cs
@@ -22,15 +22,28 @@ public class GameManager : MonoBehaviour
         timerTxt.text = _gameTimer.ToString("0.00"); 
         if (_gameTimer <= 0)
         {
-            Debug.Log("Time is up!"); 
+            Debug.Log("Time is up!");  
             _gameTimer = 0; 
         }
 
         //----- Lap Tracker -----
         if (currentLap >= _gameLaps)
         {
-            Debug.Log("Round is done :)"); 
+            Debug.Log("Round is done :)");
+            float roundFinishTime = _gameTimer; 
             // Then, check what time they got and see what star they got
+            if (roundFinishTime >= 0 && roundFinishTime <= 10)
+            {
+                Debug.Log("You got 1 star");
+            }
+            else if (roundFinishTime <= 11 && roundFinishTime <= 20)
+            {
+                Debug.Log("You get 2 stars");
+            }
+            else
+            {
+                Debug.Log("You got 3 stars!");
+            }
         }
         lapsTxt.text = currentLap.ToString() + "/" + _gameLaps.ToString();
     }
