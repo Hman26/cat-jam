8ee8495a93866632d8d87a6b79af44f74b5f59a0
CrowHeath <132368815+CrowHeath@users.noreply.github.com>
2024-08-23 21:35:19 -0400

    Kitchen Table and Cat Score Anim
    Last of the kitchen assets got added.
    I also made the control script for the cat score animation. So that just needs to be added to that UI

---
+++ LTS-Cat-GameJam/Assets/Scripts/CatScoreCheck.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScoreCheck : MonoBehaviour
+{
+    [Header("Animation Variables")]
+    public Animator catScoreAnimator;
+
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //you got three star, and plays smug cat animation
+        if (gm._goldStarTwo.activeInHierarchy && gm._goldStarOne.activeInHierarchy)
+        {
+            catScoreAnimator.SetBool("0Star", false);
+            catScoreAnimator.SetBool("1Star", false);
+            catScoreAnimator.SetBool("2Star", false);
+            return;
+        }
+        //you got two star, and plays sleeping cat animation
+        else if (gm._goldStarThree.activeInHierarchy)
+        {
+            catScoreAnimator.SetBool("0Star", false);
+            catScoreAnimator.SetBool("2Star", false);
+            catScoreAnimator.SetBool("3Star", false);
+            return;
+        }
+        //you got one star, and plays sad cat animation
+        else
+        {
+            catScoreAnimator.SetBool("1Star", false);
+            catScoreAnimator.SetBool("2Star", false);
+            catScoreAnimator.SetBool("3Star", false);
+            return;
+        }
+    }
+}
